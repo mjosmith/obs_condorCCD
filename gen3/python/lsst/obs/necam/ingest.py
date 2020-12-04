d289a16f93c5c5bd5b0d9b2f8775e8327a122009
mjosmith <61067845+mjosmith@users.noreply.github.com>
2020-12-03 23:47:55 -0500

    Update ingest.py

--- gen3/python/lsst/obs/necam/ingest.py
+++ gen3/python/lsst/obs/necam/ingest.py
@@ -1,8 +1,8 @@
 from lsst.obs.base.ingest import RawFileData
 import lsst.obs.base
-from ._instrument import NeCam
+from ._instrument import CondorCCD
 
-class NeCamRawIngestTask(lsst.obs.base.RawIngestTask):
+class CondorCCDRawIngestTask(lsst.obs.base.RawIngestTask):
 
     def extractMetadata(self, filename: str) -> RawFileData:
 
@@ -11,7 +11,7 @@ class NeCamRawIngestTask(lsst.obs.base.RawIngestTask):
         header = fitsData.readMetadata()
         datasets.append(self._calculate_dataset_info(header, filename))
 
-        instrument = NeCam()
+        instrument = CondorCCD()
         FormatterClass = instrument.getRawFormatter(datasets[0].dataId)
 
         return RawFileData(datasets=datasets, filename=filename,
