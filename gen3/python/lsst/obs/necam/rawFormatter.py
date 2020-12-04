a926f0be59fb6ab67798c1c06986269bec18c14c
mjosmith <61067845+mjosmith@users.noreply.github.com>
2020-12-03 23:49:39 -0500

    Update rawFormatter.py

--- gen3/python/lsst/obs/necam/rawFormatter.py
+++ gen3/python/lsst/obs/necam/rawFormatter.py
@@ -1,18 +1,18 @@
 from lsst.obs.base import FitsRawFormatterBase
-from ._instrument import NeCam
-from .necamFilters import NECAM_FILTER_DEFINITIONS
+from ._instrument import CondorCCD
+from .necamFilters import CONDOR_FILTER_DEFINITIONS
 # Comment-out the following line if you put .translators/necam.py in the 
 # astro_metadata_translator repository: 
-from .translators import NeCamTranslator
+from .translators import CondorCCDTranslator
 # ...and uncomment the following:
 # from astro_metadata_translator import NeCamTranslator
 
-class NeCamRawFormatter(FitsRawFormatterBase):
+class CondorCCDRawFormatter(FitsRawFormatterBase):
     """
     Gen3 Butler formatter for NeCam raw data.
     """
-    translatorClass = NeCamTranslator
-    filterDefinitions = NECAM_FILTER_DEFINITIONS
+    translatorClass = CondorCCDTranslator
+    filterDefinitions = CONDOR_FILTER_DEFINITIONS
 
     def getDetector(self, id):
-        return NeCam().getCamera()[id]
+        return CondorCCD().getCamera()[id]
